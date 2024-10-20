9f1504240aed2871f6809e579f9bd9e0d8fd6775
faram <romanchykov.webdev@gmail.com>
2024-10-20 15:02:00 +0200

    Add custom font - Add google font Amatic

--- App.tsx
+++ App.tsx
@@ -1,12 +1,37 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {StyleSheet, Text, View, FlatList, ActivityIndicator} from 'react-native';
 import DayListItem from "./src/components/core/DayListItem";
+import {Inter_900Black, useFonts} from '@expo-google-fonts/inter';
+import {useEffect} from "react";
+import * as SplashScreen from 'expo-splash-screen';
+import {AmaticSC_400Regular,AmaticSC_700Bold} from "@expo-google-fonts/amatic-sc"
+
+SplashScreen.preventAutoHideAsync()
+
+const days = [...Array(24)].map((val, index) => index + 1)
+// [...Array(24)] :Создает новый массив с длиной 24, но без значений (пустой).
+// .map((val, index) => index+1) Мы берём индекс каждого элемента и прибавляем к нему 1 (чтобы получить числа от 1 до 24).
 
 export default function App() {
 
-    const days = [...Array(24)].map((val, index) => index + 1)
-    // [...Array(24)] :Создает новый массив с длиной 24, но без значений (пустой).
-    // .map((val, index) => index+1) Мы берём индекс каждого элемента и прибавляем к нему 1 (чтобы получить числа от 1 до 24).
+
+
+    // add google fonts
+    const [loaded, error] = useFonts({
+        Inter: Inter_900Black,
+        Amatic:AmaticSC_400Regular,
+        AmaticBold:AmaticSC_700Bold,
+    })
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !error) {
+        return null;
+    }
+    // add google fonts
 
     // console.log('hello')
     return (
@@ -17,7 +42,7 @@ export default function App() {
                 contentContainerStyle={styles.content}
                 columnWrapperStyle={styles.column}
                 renderItem={({item}) => (
-                    <DayListItem day={item} />
+                    <DayListItem day={item}/>
                 )}
             />
 
